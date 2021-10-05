22fb9e999657cfc73c7e2ae5978050b12b18f418
Danylo Kolohryvko <yoodenkol@gmail.com>
2021-10-05 12:48:09 +0300

    Add project files.

---
+++ DepositIdentity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DepositIdentity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
